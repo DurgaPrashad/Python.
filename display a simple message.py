fffad0fb7646fdfb7ba6d9e95040f0b233b93a50
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-08-13 11:43:29 +0530

    Create display a simple message.py

---
+++ display a simple message.py
@@ -0,0 +1,19 @@
+#Aim: Write a python script to display a simple message.
+# """Algorithm:
+# Step1. START 
+# Step2. PRINT WELCOME TO PYTHON PROGRAMING 
+# Step3. PRINT ‘IAM STUDING SVCET(A), CHITTOOR AP’ 
+# Step4. MSG=INPUT FROM USER, ENTER MESSAGE 
+# Step5. PRINT MSG 
+# Step6. STOP 
+# Source Code:"""
+
+print('Hello World') 
+print('Welcome to Python Programming') 
+print('I study in SVCET(A),Chittoor,AP') 
+# Output:
+# Hello World 
+# Welcome to Python Programming 
+# I study in SVCET(A),Chittoor,AP 
+# Result:
+# Successfully completed to display a simple message
