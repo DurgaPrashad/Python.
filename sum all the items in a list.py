80f9b064e261c2faf5ecd0c51eeff15c6bfee945
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-08-22 10:33:27 +0530

    Create sum all the items in a list.py

---
+++ sum all the items in a list.py
@@ -0,0 +1,25 @@
+# Aim: Write a Python program to calculate sum all the items in a list. 
+# i. To calculate sum all the items in a list. list1=[1,2,3,4,5] 
+# Algorithm: 
+# 1.Start the program. 
+# 2.Create a list of numbers and store it in a variable named "numbers". 
+# 3.Initialize a variable named "total" to 0, which will hold the sum of the 
+# numbers. 
+# 4.Iterate through each number in the list using a for loop: 
+# - Inside the loop, add the current number to the "total" variable. 
+# 5.After the loop, "total" will contain the sum of all the numbers in the list. 
+# 6.Print the value of "total" as the sum of all the items in the list. 
+# 7.End the program
+# Source code: 
+
+List1=[1,2,3,4,5] 
+Sum=0 
+for x in list1:
+sum+=x 
+print('Displaying List',list1) 
+print('Sum of all items in the list',sum) 
+
+# Output: 
+# Displaying List [1, 2, 3, 4, 5] 
+# Sum of all items in the list 15 
+# RESULT: To calculate sum all the items in a list executed successfully
