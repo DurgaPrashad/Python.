006ade57c532971a0647eb7f1a50077a0c8af803
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-09-02 12:04:56 +0530

    Create demonstrate string methods.py

---
+++ demonstrate string methods.py
@@ -0,0 +1,24 @@
+# Aim: Write a python script to demonstrate string methods 
+# Algorithm: 
+# Step 1: Initialize a string variable. 
+# Step 2: Use string methods to perform different operations on the string. 
+# a.Use the upper() method to convert the string to uppercase. 
+# b.Use the lower() method to convert the string to lowercase. 
+# c.Use the capitalize() method to capitalize the first letter of the string. 
+# d.Use the count() method to count the occurrences of a substring in the string. 
+# e.Use the replace() method to replace a substring with another substring. 
+# f.Use the split() method to split the string into a list of substrings. 
+# Step 3: Display the results of the string operations. 
+# SOURCE CODE: 
+# enumerate() 
+str = 'apple' 
+list_enumerate = list(enumerate(str)) 
+print('list(enumerate(str) = ', list_enumerate) 
+# Output: 
+# list(enumerate(str) = [(0, 'a'), (1, 'p'), (2, 'p'), (3, 'l'), (4, 'e')] 
+# #character count str='football' 
+print('len(str) = ', len(str)) 
+
+# Output: 
+# len(str) = 8 
+# Result: python script to demonstrate string methods executed successfully
