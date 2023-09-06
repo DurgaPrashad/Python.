2aade4d9be966887ff2622847de8ea4e15f06946
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-09-06 16:27:06 +0530

    Create reverse a string Sample String.py

---
+++ reverse a string Sample String.py
@@ -0,0 +1,21 @@
+# AIM: Write a Python program to reverse a string Sample String : “1234abcd” 
+# Expected Output : “dcba4321 
+# Sample String : "1234abcd" 
+# Expected Output : "dcba4321" 
+# Algorithm:
+# Step 1: Prompt the user to enter a string. 
+# Step 2: Initialize an empty string to store the reversed string. 
+# Step 3: Use a loop to iterate through each character in the string starting from 
+# the last character. 
+# - Within the loop, append each character to the reversed string. 
+# Step 4: Display the reversed string. 
+# Source code: 
+
+str=input('Enter a string: ') 
+print(str[::-1]) 
+
+# Output: 
+# Enter a string: 1234abcd 
+# dcba4321 
+# RESULT: Python program to reverse a string Sample String : “1234abcd” 
+# Expected Output : “dcba4321” Executed successfully. 
