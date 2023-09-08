1116a102daf4cdcdd92d8d332d3e6a967dd8ef39
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-09-08 07:38:20 +0530

    Create upper and lower case .py

---
+++ upper and lower case .py
@@ -0,0 +1,23 @@
+# AIM: Write a Python script that takes input from the user and displays that 
+# input back in upper and lower case 
+# Algorithm: 
+# Step 1: Prompt the user to enter a string. 
+# Step 2: Read the input from the user and store it in a variable. 
+# Step 3: Use the upper() method to convert the string to uppercase and store it in 
+# an uppercase variable. 
+# Step 4: Use the lower() method to convert the string to lowercase and store it in 
+# a lowercase variable. 
+# Step 5: Display the input string, the uppercase version, and the lowercase 
+# version to the user.
+# Source code: 
+
+str=input('Enter a string: ') 
+print('Lower Case: ',str.lower()) 
+print('Upper Case: ',str.upper()) 
+
+# Output: 
+# Enter a string: Apple is a Fruit 
+# Lower Case: apple is a fruit 
+# Upper Case: APPLE IS A FRUIT 
+# RESULT: Python script that takes input from the user and displays that input 
+# back in upper and lower cases Executed successfully
