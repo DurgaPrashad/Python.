ea273fda140534859816dacda9c46f2acf9b2171
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-09-09 07:11:32 +0530

    Create mean, median,  mode.py

---
+++ mean, median,  mode.py
@@ -0,0 +1,26 @@
+# AIM: Write a program to compute summary statistics such as mean, median, 
+# mode, standard deviation and variance of the given different types of data.
+# Algorithm: 
+# Import the necessary libraries: 
+# import numpy as np from 
+# scipy import stats 
+# compute the mean: 
+# mean = np.mean(data) 
+# print("Mean:", mean) 
+# compute the median: 
+# median = np.median(data) 
+# print("Median:", median) 
+# compute the mode: 
+# mode = stats.mode(data) 
+# print("Mode:", mode.mode) compute the standard diviation: std_dev = 
+# np.std(data) 
+# print("Standard Deviation:", std_dev) 
+# Source code: 
+
+import numpy as np 
+array = np.arange(10) 
+print(array) 
+
+# Output: 
+# # [0 1 2 3 4 5 6 7 8 9] 
+# # mean
